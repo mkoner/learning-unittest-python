079feee7ea54f4480ac64dc4431b1cdb217eb122
mkoner <amzid@hotmail.fr>
2023-02-26 23:42:30 +0000

    Written unittest for other methods

--- test_calc.py
+++ test_calc.py
@@ -14,6 +14,27 @@ class TestCalc(unittest.TestCase):
         self.assertEquals(calc.add(1, -3), -2)
         self.assertEquals(calc.add(-1, -3), -4)
 
+    def test_subtract(self):
+        """Test the subtract function"""
+        self.assertEquals(calc.subtract(12, 4), 8)
+        self.assertEquals(calc.subtract(1, -3), 4)
+        self.assertEquals(calc.subtract(-1, -3), 2)
+
+    def test_multiply(self):
+        """Test the add function"""
+        self.assertEquals(calc.multiply(12, 4), 48)
+        self.assertEquals(calc.multiply(1, -3), -3)
+        self.assertEquals(calc.multiply(-1, -3), 3)
+    
+    def test_divide(self):
+        """Test the add function"""
+        self.assertEquals(calc.divide(12, 4), 3)
+        self.assertEquals(calc.divide(0, 4), 0)
+        self.assertEquals(calc.divide(5, 2), 2.5)
+
+        #self.assertRaises(ValueError, calc.divide, 10, 0)
+        with self.assertRaises(ValueError):
+            calc.divide(10, 0)
 
 if __name__ == '__main__':
     unittest.main()
